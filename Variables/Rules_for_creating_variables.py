0c93ffa7c9af6591337582db6cf752535204c3b9
tejpol <tpolava@mail.com>
2019-06-12 19:38:03 -0500

    Variables Introduction

---
+++ Variables/Rules_for_creating_variables.py
@@ -0,0 +1,51 @@
+#/usr/bin/python
+
+# Shebang line - #/usr/bin/python
+
+#Variables Rules
+
+# A-Z
+# a-z
+# combinations of A-Z & a-z
+# 0-9
+# combinations of A-Z, a-z & 0-9
+# an underscore(_)
+# combinations of A-Z, a-z, 0-9 & _
+
+a = 1
+B = 2
+cD = 3
+EfGh = 4
+iJkl_Mno = 5
+_pqr = 6
+
+"""
+Data Types:
+
+Numbers
+Strings
+List
+Tuple
+Dict
+Set
+
+int()
+str()
+list()
+tuple()
+dict()
+set()
+"""
+
+
+# Numbers DataType
+
+a = 10
+
+b = 10.75
+
+c = 3 + 4j #Could use upper case J as well
+
+print(a)
+print(b)
+print(c)
